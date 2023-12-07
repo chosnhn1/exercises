9163b9964b75991a7c9f97e5b24c0b82b8b4f066
Cho Seonghan <chosnhn1@gmail.com>
2023-12-07 23:10:15 +0900

    counter

--- sql/00_quick_recap.sql
+++ sql/00_quick_recap.sql
@@ -19,7 +19,7 @@ WHERE CustomerID = 32;
 SELECT * FROM Customers
 WHERE City = 'Berlin'
 AND PostalCode = 12209;
-
+                                                           
 SELECT * FROM Customers
 WHERE City = 'Berlin'
 OR City = 'London';
